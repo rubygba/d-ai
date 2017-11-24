8c458a35d9eddf3f44e25c69ad8363795c783d66
rubygba <rubygba@foxmail.com>
2017-11-24 18:38:13 +0800

    init

---
+++ src/routes/Users.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { connect } from 'dva'
+import styles from './Users.css'
+import UsersComponent from '../components/Users/Users'
+
+function Users() {
+  return (
+    <div className={styles.normal}>
+      <UsersComponent></UsersComponent>
+    </div>
+  )
+}
+
+function mapStateToProps() {
+  return {}
+}
+
+// export default connect(mapStateToProps)(Users)
+export default connect()(Users)
